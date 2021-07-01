dba2410216710bfb4cdd96e456edbaed48b9309d
odinsam <>
2021-07-01 09:07:38 +0800

    ✨ feat(OdinCore/ConfigModel): 项目启动，配置文件加载核心类
    加载核心 cnf.json 并 按照当前环境变量 开始加载对应文件夹下所有的配置文件

---
+++ OdinCore/Configs/ConfigHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Odin.Plugs.OdinCore.Configs
+{
+    public static class ConfigHelper
+    {
+        private static readonly IConfiguration _configuration;
+
+        static ConfigHelper()
+        {
+            //在当前目录或者根目录中寻找appsettings.json文件
+            var fileName = "ServiceConfig/cnf.json";
+
+            var directory = AppContext.BaseDirectory;
+            directory = directory.Replace("\\", "/");
+
+            var filePath = $"{directory}/{fileName}";
+            if (!File.Exists(filePath))
+            {
+                var length = directory.IndexOf("/bin", StringComparison.Ordinal);
+                filePath = $"{directory.Substring(0, length)}/{fileName}";
+            }
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile(filePath, false, true);
+
+            _configuration = builder.Build();
+        }
+
+        public static IConfigurationSection GetSection(string key)
+        {
+            return _configuration.GetSection(key);
+        }
+
+        public static T LoadConfig<T>(string configFullPath)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile(configFullPath).Build();
+            T config = builder.Get<T>();
+            return config;
+        }
+
+        public static T GetConfig<T>(string configPath, string fileName)
+        {
+            foreach (var item in Directory.GetFiles(configPath))
+            {
+                if (Path.GetFileName(item.ToLower()) == fileName.ToLower())
+                {
+                    return ConfigHelper.LoadConfig<T>(item);
+                }
+            }
+            return default(T);
+        }
+    }
+}
