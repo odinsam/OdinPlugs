24f0848e1ca0789f795500736287d2ba765d39ce
odinsam <>
2021-07-01 09:02:43 +0800

    ✨ feat(OdinCore/ConfigModel): errorCode 错误码映射

---
+++ OdinCore/ConfigModel/ErrorCodeModel/ErrorCodeCnfOptions.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace Odin.Plugs.OdinCore.ConfigModel.ErrorCodeModel
+{
+    public class ErrorModel
+    {
+        /// <summary>
+        /// ok
+        /// </summary>
+        public string ErrorCode { get; set; }
+        /// <summary>
+        /// ok
+        /// </summary>
+        public string ShowMessage { get; set; }
+        public string Handle { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ErrorMessage { get; set; }
+    }
+
+    public class ErrorCodesCnfOptions
+    {
+        public List<ErrorModel> ErrorCodes { get; set; }
+    }
+}
