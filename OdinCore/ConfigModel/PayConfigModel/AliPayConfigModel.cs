aebd914e9969dd621e76b50bfe550736e79b5298
odinsam <>
2021-07-01 09:04:02 +0800

    ✨ feat(OdinCore/ConfigModel): alipay 阿里支付模型对象映射

---
+++ OdinCore/ConfigModel/PayConfigModel/AliPayConfigModel.cs
@@ -0,0 +1,25 @@
+namespace Odin.Plugs.OdinCore.ConfigModel.PayConfigModel
+{
+    public class CertPath_Model
+    {
+        public string AppPublicKeyPath { get; set; }
+        public string AlipayPublicKeyPath { get; set; }
+        public string AliRootPath { get; set; }
+
+    }
+    public class AliPayCnfOptions
+    {
+        public string AppPublicKey { get; set; }
+        public string AppId { get; set; }
+        public string CharSet { get; set; }
+        public string Gatewayurl { get; set; }
+        public string PrivateKey { get; set; }
+        public string SignType { get; set; }
+        public string AlipayPublicKey { get; set; }
+        public string AliNotifyUrl { get; set; }
+        public string AliReturnUrl { get; set; }
+        public string AliFormat { get; set; }
+        public string Uid { get; set; }
+        public CertPath_Model CertPath { get; set; }
+    }
+}
