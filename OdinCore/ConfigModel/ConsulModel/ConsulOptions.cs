d699f38c86195e4bdbc8f37e6531643e08e44e55
odinsam <>
2021-07-01 09:02:20 +0800

    ✨ feat(OdinCore/ConfigModel): consul 配置映射

---
+++ OdinCore/ConfigModel/ConsulModel/ConsulOptions.cs
@@ -0,0 +1,26 @@
+namespace Odin.Plugs.OdinCore.ConfigModel.ConsulModel
+{
+    public class CheckOption
+    {
+        public int DeregisterCriticalServiceAfter { get; set; }
+        public string HealthApi { get; set; }
+        public int Interval { get; set; }
+        public int Timeout { get; set; }
+    }
+    public class ConsulOptions
+    {
+        public bool Enable { get; set; }
+        public string Protocol { get; set; }
+        public string ConsulName { get; set; }
+        public string ConsulIpAddress { get; set; }
+        public int ConsulPort { get; set; }
+        public string DataCenter { get; set; }
+        /// <summary>
+        /// 服务器权重
+        /// </summary>
+        /// <value></value>
+        public int Weight { get; set; } = 50;
+        public CheckOption ConsulCheck { get; set; }
+
+    }
+}
