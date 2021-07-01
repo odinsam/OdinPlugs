91fdf26e8a12e6aca2f3f3b4168a39e7abc73fa0
odinsam <>
2021-07-01 12:18:14 +0800

    ✨ feat(OdinExtensions/BasicExtensions): number类型扩展操作

---
+++ OdinExtensions/BasicExtensions/OdinNum/NumExtensions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdinPlugs.OdinExtensions.BasicExtensions.OdinNum
+{
+    public class NumExtensions
+    {
+
+    }
+}
