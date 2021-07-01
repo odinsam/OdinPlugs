c28a11096b2a782c0ef2617e66af7928b1b45725
odinsam <>
2021-07-01 09:05:50 +0800

    ✨ feat(OdinCore/ConfigModel): wxApi 相关模型对象映射

---
+++ OdinCore/ConfigModel/WxParamsModel/MAEParamModel.cs
@@ -0,0 +1,17 @@
+namespace Odin.Plugs.OdinCore.ConfigModel.WxParamsModel.Core
+{
+    public class MAEParamsModel
+    {
+        public string Token { get; set; }
+        public string AccessToken { get; set; }
+        public string EncodingAESKey { get; set; }
+        public string AppID { get; set; }
+        public string Signature { get; set; }
+        public string Timestamp { get; set; }
+        public string Nonce { get; set; }
+        public string Openid { get; set; }
+        public string Encrypt_type { get; set; }
+        public string Msg_Signature { get; set; }
+        public string Content { get; set; }
+    }
+}
