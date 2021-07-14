22b08898d4bd36fd18f80689def06b87fbcd92af
‘odinsam’ <123679@qq.com>
2021-07-14 18:34:44 +0800

    fix(ControllerExtends): 🐞 移除 T GetDIServices<T>(this Controller controller)

--- OdinMvcCore/OdinExtensions/ControllerExtends.cs
+++ OdinMvcCore/OdinExtensions/ControllerExtends.cs
@@ -9,10 +9,6 @@ namespace OdinPlugs.OdinMvcCore.OdinExtensions
 
     public static class ControllerExtends
     {
-        public static T GetDIServices<T>(this Controller controller)
-        {
-            return controller.HttpContext.RequestServices.GetRequiredService<T>();
-        }
         public static RequestParamsModel GetRequestParams(this Controller controller)
         {
             return OdinRequestParamasHelper.GetRequestParams(controller);
