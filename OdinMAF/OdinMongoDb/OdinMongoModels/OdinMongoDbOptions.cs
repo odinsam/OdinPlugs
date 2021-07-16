797692cf5f53262fe804cd607cd74531890a5c05
‘odinsam’ <123679@qq.com>
2021-07-16 15:32:49 +0800

    🦄 refactor(*): 移动注入到项目 OdinPlugs.OdinInject

--- OdinMAF/OdinMongoDb/OdinMongoModels/OdinMongoDbOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace OdinPlugs.OdinMAF.OdinMongoDb.OdinMongoModels
-{
-    public class OdinMongoDbOptions
-    {
-        public string ConnectionString { get; set; }
-        public string DbName { get; set; }
-    }
-}
